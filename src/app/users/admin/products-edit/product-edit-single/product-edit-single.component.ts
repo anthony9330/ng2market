974cc8f10be4b25503cad08a0654cb3f9c872361
taras <anthony9330t@gmail.com>
2017-10-20 00:49:46 +0300

    make dynamic additional fields for categories in products edit page

--- src/app/users/admin/products-edit/product-edit-single/product-edit-single.component.ts
+++ src/app/users/admin/products-edit/product-edit-single/product-edit-single.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter } from '@angular/core';
 import {ActivatedRoute,Params, Router, Data} from "@angular/router";
 import {FormGroup,FormControl,Validators,FormArray,FormBuilder} from "@angular/forms";
 import {UsersService} from "../../../users.service";
@@ -19,13 +19,16 @@ export class ProductEditSingleComponent implements OnInit {
               public fb: FormBuilder,) { }
   id:number;
   productForm:FormGroup;
-  imagesPath:any;
   product:Product;
   categories:Category[];
+  catChngd:EventEmitter<any>= new EventEmitter();
+  catFields;
+ 
 
   ngOnInit() {
    this.route.params.subscribe((
      params:Params)=>{
+      this.initForm();
        this.id=+params['id'];    
        this.userService.getProducts().subscribe((products)=>{
           
@@ -34,19 +37,71 @@ export class ProductEditSingleComponent implements OnInit {
               this.categories=categories;
               console.log(categories);
                this.product=this.userService.getProduct(this.id);
-                console.log(this.product);
+               this.getAddFields(this.product.categoryID);
+                // console.log(this.product);
+                
+
                 this.setForm();
+
+                 // this.catChngd.subscribe((id)=>{
+
+                 //   console.log(id);
+                 //     this.getAddFields(id);
+                 //     this.setForm();
+                 //      console.log("categories changed id");
+                 //  console.log(id);
+                  
+                 //     // this.setForm();
+                 // });
+
+
+
+               
+                  console.log( this.productForm);
+
+
+                 this.productForm.controls['categories'].valueChanges.subscribe((id)=>{
+                    this.getAddFields(id);
+                    console.log("categories changed id");
+                  console.log(id);
+                     // this.setForm();
+                      // this.productForm.controls['additionalFields'].patchValue([1,2]);
+
+                      this.productForm.controls['additionalFields']= this.setFields(); 
+                 });
+               
+
           });
-     
-         
        });
- this.initForm();
-          
-    
+      
+
 
    });
 
   }
+
+  getAddFields(id:number){
+      this.categories.forEach((category_,i,marr)=>{
+            if(category_['id']==id){
+              this.catFields=JSON.parse(category_.additionalFields);
+            }
+           
+      });
+
+      console.log("product: ");
+      console.log(this.product);
+
+  }
+
+
+  categoryChange(id:any){
+   this.catChngd.emit(id);
+    console.log(id);
+
+
+  }
+
+
 
 
 
@@ -61,23 +116,33 @@ export class ProductEditSingleComponent implements OnInit {
         'price':new FormControl('',Validators.required),
         'description':new FormControl('',Validators.required),
         'location':new FormControl('',Validators.required),
-        'additionalFields':new FormControl('',Validators.required),
+        'additionalFields':new FormArray([]),
         'approved':new FormControl('',Validators.required),
       })
  
 
     }
 
+    setFields():any{
+      let addFieldArr= new FormArray([]);
+        this.catFields.forEach(function(category_,i,arr){
+            addFieldArr.push(new FormControl(category_));
+           
+        });
+        console.log(addFieldArr);
+
+        return addFieldArr;
+    }
+
+//SETS INIZIALIZED BEFORE VALUES TO FORM
     setForm(){
-        this.productForm=new FormGroup({
-          'title':new FormControl(this.product.title,Validators.required),
-          'categories':new FormControl(this.product.categoryID,Validators.required),
-          'price':new FormControl(this.product.price,Validators.required),
-          'description':new FormControl(this.product.description,Validators.required),
-          'location':new FormControl(this.product.location,Validators.required),
-          'additionalFields':new FormControl(this.product.additionalFields,Validators.required),
-          'approved':new FormControl(this.product.approved,Validators.required),
-        })
+        this.productForm.controls['title'].patchValue(this.product.title);
+        this.productForm.controls['categories'].patchValue(this.product.categoryID);
+        this.productForm.controls['price'].patchValue(this.product.price);
+        this.productForm.controls['description'].patchValue(this.product.description);
+        this.productForm.controls['location'].patchValue(this.product.location);
+       this.productForm.controls['additionalFields']= this.setFields(); 
+        this.productForm.controls['approved'].patchValue(this.product.approved);
     }
 
 
